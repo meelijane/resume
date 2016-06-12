92afdbd1d54a86088f8bfeb472fa3bab2d212936
Amelia Schmidt <amelia.schmidt@equiem.com.au>
2016-06-12 23:52:32 +1000

    add people

---
+++ src/cljs/people/config.cljs
@@ -0,0 +1,133 @@
+(ns people.config)
+(def data
+  [{:person/id :gab
+    :name "Gabrielle McMillan"
+    :position "CEO"
+    :workplace :equiem}
+   {:person/id :dave
+    :name "David Meister"
+    :position "Co-founder"
+    :workplace :ew}]
+  {:person/id :josh
+   :name "Joshua Hardy"
+   :position "Director"
+   :workplace :godel}
+  {:person/id :ash
+   :name "Ashley Sambrooks"
+   :position "Director"
+   :workplace :godel}
+  {:person/id :robyn
+   :name "Robyn Wilson"
+   :position "Director"
+   :workplace :freelance}
+  {:person/id :jaymz
+   :name "Jaymz Clements"
+   :position "Music Editor"
+   :workplace :furst}
+  {:person/id :emily
+   :name "Emily Doyle"
+   :position "Project Manager"
+   :workplace :godel
+   :testimonial "Amelia is a great addition to any team who cares about quality, collaboration and innovation. She brings to her work a passion for well-formed, elegant code, and is always open to both sharing her expertise with her team members and seeking ideas from colleagues on how to find the best solution for a problem.
+Amelia naturally gravitates to leadership roles, speaking up where she sees room for improvement and contributing to forming processes to ensure everyone can work better. Her sense of responsibility and dedication to follow through is a valuable asset to any team she's a part of.
+In addition to the above, her technical skills are superb, and her passion for continuous learning will ensure they remain so well into her career. She can always be trusted to deliver on time and is the ultimate team player; with Amelia on your project team success is pretty much guaranteed."}
+  {:person/id :kris
+   :name "Kris Furst"
+   :position "Director"
+   :workplace :furst}
+  {:person/id :lou
+   :name "Lou Pardi"
+   :position "Contributor"
+   :workplace :furst
+   :testimonial "Amelia is a diligent, focused, competent and efficient editor and a talented writer. She is outstanding under pressure and a joy to work with. Amelia's work ethic is second to none."}
+  {:person/id :jack
+   :name "Jack Nicholas"
+   :position "Director"
+   :workplace :godel}
+  {:person/id :peter
+   :name "Peter Macks"
+   :position "Principal"
+   :workplace :godel}
+  {:person/id :grant
+   :name "Grant Barnes"
+   :position "Manager"
+   :workplace :worldbar}
+  {:person/id :luke
+   :name "Luke Anton"
+   :position "Director"
+   :workplace :godel}
+  {:person/id :greenie
+   :name "Andrew Green"
+   :position "Director"
+   :workplace :worldbar
+   :testimonial "Apart from having a vibrant personality that's matched only by her vibrant red hair Amelia is a very talented and skilled writer - she's also very well spoken.}) 
+Amelia always seems to know what's cool before you but she'll never make you feel inadequate about it, instead she'll genuinely be excited to introduce you to something new.
+Working in the same office as Amelia was an absolute pleasure and I wish that more people were as socially pleasant and passionate about their career as Amelia."}
+  {:person/id :mikey
+   :name "Michael Carr"
+   :position "Managing Editor"
+   :workplace :freelance}
+  {:person/id :andy
+   :name "Andy Henson"
+   :position "Director"
+   :workplace :godel}
+  {:person/id :leon
+   :name "Leon Meyer"
+   :position "Senior Communication Officer"
+   :workplace :godel}
+  {:person/id :donna
+   :name "Donna Benjamin"
+   :position "Project Manager"
+   :workplace :drupal}
+  {:person/id :sandy
+   :name "Sandra Leong"
+   :position "Yoga Teacher"
+   :workplace :freelance}
+  {:person/id :dc
+   :name "Davperson/id Chiapoco"
+   :position "Head of Marketing"
+   :workplace :equiem}
+  {:person/id :kelly
+   :name "Kelly Kozaris"
+   :position "Co-organiser"
+   :workplace :freelance}
+  {:person/id :pip
+   :name "Pip Smith"
+   :position "Co-organiser"
+   :workplace :freelance}
+  {:person/id :krisr
+   :name "Kristofer Rogers"
+   :position "Director"
+   :workplace :ymi}
+  {:person/id :joshm
+   :name "Joshua Martin"
+   :position "Lead Developer"
+   :worplace :godel}
+  {:person/id :emma
+   :name "Emma Forster"
+   :position "Audience Development Producer"
+   :workplace :freelance}
+  {:person/id :emmas
+   :name "Emma Stewart"
+   :position "Production Manager"
+   :workplace :equiem}
+  {:person/id :george
+   :name "George Cockerill"
+   :position "Instructor"
+   :workplace :freelance}
+  {:person/id :tim
+   :name "Tim Levy"
+   :position "Senior Photographer"
+   :workplace :freelance}
+  {:person/id :dom
+   :name "Dom Alessio"
+   :position "Arts Editor"
+   :workplace :freelance}
+  {:person/id :steph
+   :name "Steph Harmon"
+   :position "Founder"
+   :workplace :worldbar}
+  {:person/id :rudi
+   :name "Rudi Haig"
+   :position "Contributor"
+   :workplace :ymi})
