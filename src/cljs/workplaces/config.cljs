f3e2bda35ac07906a659cc66bf8afd5ba35aebe0
Amelia Schmidt <amelia.schmidt@equiem.com.au>
2016-06-12 23:51:22 +1000

    added workplaces

---
+++ src/cljs/workplaces/config.cljs
@@ -0,0 +1,33 @@
+(ns workplace.config)
+(def data
+  [{:workplace/id :ew
+    :name "Estimate Work"
+    :website "estimate-work.com"
+    :address "1/25 Queenbserry Place, Melbourne VIC 3000"}
+   {:workplace/id :equiem
+    :name "Equiem"
+    :website "equiem.com.au"
+    :address "6/525 Collins St, Melbourne VIC 3000"}]
+  {:workplace/id :godel
+   :name "Godel"
+   :website "godel.com.au"
+   :address "3/525 Collins St, Melbourne VIC 3000"}
+  {:workplace/id :furst
+   :name "Furst Media"
+   :website "furstmedia.com.au"
+   :address "3 Newton St, Richmond VIC 3121"}
+  {:workplace/id :freelance
+   :name "Freelance"}
+  {:workplace/id :drupal
+   :name "Drupal Community"
+   :website "drupal.org.au"}
+  {:workplace/id :br
+   :name "Be Responsive"
+   :website "beresponsive.io"
+   :address "The Arena, NAB building, Level 2, 700 Bourke St Melbourne, 3000"}
+  {:workplace/id :worldbar
+   :name "The World Bar"
+   :website "theworldbar.com"
+   :address "24 Bayswater Road, Kings Cross NSW 2011"}
+  {:workplace/id :ymi
+   :name "ymi Magazine"})
