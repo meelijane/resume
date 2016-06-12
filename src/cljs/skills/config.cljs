f4ce62993435213989b85dc15924869800494060
Amelia Schmidt <amelia.schmidt@equiem.com.au>
2016-06-12 23:52:20 +1000

    add skills

---
+++ src/cljs/skills/config.cljs
@@ -0,0 +1,74 @@
+(ns skills.config)
+(def data
+  [{:skill/id :uxr
+    :name "UX research"}
+   {:skill/id :uxd
+     :name "UX design"}
+   {:skill/id :uxt
+    :name "UX testing"}
+   {:skill/id :fe
+    :name "Front-end engineering"}
+   {:skill/id :be
+    :name "Back-end engineering"}
+   {:skill/id :product
+    :name "Product management"}
+   {:skill/id :project
+    :name "Project management"}
+   {:skill/id :feature
+    :name "Feature development"}
+   {:skill/id :content
+    :name "Content strategy"}
+   {:skill/id :ia
+    :name "Information architecture"}]
+  {:skill/id :marketing
+   :name "Marketing"}
+  {:skill/id :business
+   :name "Business strategy"}
+  {:skill/id :editing
+   :name "Editing"}
+  {:skill/id :subediting
+   :name "Subediting"}
+  {:skill/id :journalism
+   :name "Journalism"}
+  {:skill/id :copywriting
+   :name "Copywriting"}
+  {:skill/id :technicalwriting
+   :name "Technical writing"}
+  {:skill/id :blogging
+   :name "Blogging"}
+  {:skill/id :management
+   :name "Management"}
+  {:skill/id :deployment
+   :name "Website deployment"}
+  {:skill/id :photography
+   :name "Photography"}
+  {:skill/id :leadership
+   :name "Leadership"}
+  {:skill/id :lesson
+   :name "Lesson development"}
+  {:skill/id :teaching
+   :name "Teaching"}
+  {:skill/id :webflow
+   :name "Webflow development"}
+  {:skill/id :Wordpress
+   :name "Wordpress development"}
+  {:skill/id :drupal
+   :name "Drupal development"}
+  {:skill/id :events
+   :name "Event co-ordination"}
+  {:skill/id :sass
+   :name "Sass"}
+  {:skill/id :javascript
+   :name "Javascript"}
+  {:skill/id :jquery
+   :name "JQuery"}
+  {:skill/id :php
+   :name "PHP"}
+  {:skill/id :hoplon
+   :name "Hoplon"}
+  {:skill/id :cljs
+   :name "Clojurescript"}
+  {:skill/id :uxpin
+   :name "UXpin"}
+  {:skill/id :indesign
+   :name "Indesign"})
